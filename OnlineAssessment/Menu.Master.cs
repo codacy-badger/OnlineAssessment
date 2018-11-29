b7ee4b374b5eb8b8e21fb20a83b2512eb01a47b0
tyrng <ongtypc@gmail.com>
2018-11-30 03:17:35 +0800

    add gender icon, add username

--- OnlineAssessment/Menu.Master.cs
+++ OnlineAssessment/Menu.Master.cs
@@ -6,6 +6,22 @@ namespace OnlineAssessment
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            String username = (String)Session["username"];
+            if (Session["gender"] != null)
+            {
+                if (Session["gender"].ToString().ToUpper().Equals("MALE"))
+                {
+                    username = "Mr. " + username;
+                }
+                else
+                {
+                    username = "Ms. " + username;
+                }
+            }
+                
+
+            userName.Text = username;
+
             Response.Cache.SetNoStore();
 
             String activepage = Request.RawUrl;
